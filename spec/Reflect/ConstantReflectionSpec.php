ddbcd07d684637a28d86f955e706641202311da2
James Benner <james.benner@gmail.com>
2016-03-04 18:58:13 -0500

    Added `it_should_have_a_property_named_name_with_a_value_of_e_error()` test.

--- spec/Reflect/ConstantReflectionSpec.php
+++ spec/Reflect/ConstantReflectionSpec.php
@@ -7,8 +7,18 @@ use Prophecy\Argument;
 
 class ConstantReflectionSpec extends ObjectBehavior
 {
+    function let()
+    {
+        $this->beConstructedWith('E_ERROR');
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('RadHam\Reflect\ConstantReflection');
+    }
+
+    function it_should_have_a_property_named_name_with_a_value_of_e_error()
+    {
+        $this->name->shouldBe('E_ERROR');
     }
 }
--- src/Reflect/ConstantReflection.php
+++ src/Reflect/ConstantReflection.php
@@ -4,4 +4,11 @@ namespace RadHam\Reflect;
 
 class ConstantReflection
 {
+    /**
+     * @param string $name
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
 }
