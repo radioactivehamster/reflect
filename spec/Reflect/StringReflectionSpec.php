f6378b50da3ecaf251c26a32643901d968b40598
James Benner <james.benner@gmail.com>
2016-03-18 19:43:05 -0400

    Added `it_should_have_a_property_named_type_with_a_value_of_string()` spec.

--- spec/Reflect/StringReflectionSpec.php
+++ spec/Reflect/StringReflectionSpec.php
@@ -7,8 +7,18 @@ use Prophecy\Argument;
 
 class StringReflectionSpec extends ObjectBehavior
 {
+    function let()
+    {
+        $this->beConstructedWith('Hello world!');
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('RadHam\Reflect\StringReflection');
+    }
+
+    function it_should_have_a_property_named_type_with_a_value_of_string()
+    {
+        $this->type->shouldBe('string');
     }
 }
--- src/Reflect/StringReflection.php
+++ src/Reflect/StringReflection.php
@@ -2,6 +2,17 @@
 
 namespace RadHam\Reflect;
 
-class StringReflection
+class StringReflection extends AbstractReflection
 {
+    /** @var string */
+    protected $value;
+
+    /**
+     * @param string $value
+     */
+    public function __construct($value)
+    {
+        $this->type  = 'string';
+        $this->value = $value;
+    }
 }
